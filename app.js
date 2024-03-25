74f6572393dd590b5954fc7da9efd7e2b369c906
Nicolas Barrows <contact@nicolasbarrows.com>
2024-03-25 14:48:54 -0500

    EXERCISE 1-3 COMPLETE

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-// TODO: Enable strict mode
+"use strict";
 
-// TODO: Fix the following parameter list
-function parseToJSON(data, data) {
-  // TODO: Add a try/catch block to
-  // attempt to convert 'data' to JSON
-  return JSON.parse(data); 
-  
-  // TODO: if an exception is raised
-  // print the error to the console
-  // and return null
+function parseToJSON(data) {
+  try { 
+    return JSON.parse(data); 
+  } catch (error) {
+    console.error("invalid input. parseToJSON expects JSON");
+    return null;
+  }
 }
 
 let failData = "<h1>Hello World!</h1>"
